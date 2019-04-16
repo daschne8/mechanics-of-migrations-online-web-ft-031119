dc0b8c366032cc363f2b8a376212328ab8a479f3
Daniel Schneider <daschne8@gmail.com>
2019-04-16 17:55:25 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,6 +1,6 @@
 # db/migrate/02_add_favorite_food_to_artists.rb
 
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.1]
   def change
     add_column :artists, :favorite_food, :string
   end
